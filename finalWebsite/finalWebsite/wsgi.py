cc3fa2b4ca30c85bb18546db35be1f83d5f582ec
Useer923 <thomastbnlbs@gmail.com>
2024-07-23 16:25:15 +0200

    up-to-date for Render wsgi.py

--- finalWebsite/finalWebsite/wsgi.py
+++ finalWebsite/finalWebsite/wsgi.py
@@ -16,26 +16,26 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalWebsite.settings')
 
 application = get_wsgi_application()
 
-directory = 'finalWebsite/appartement/RandomForestRegressor/models'
+directory = './finalWebsite/appartement/RandomForestRegressor/models'
 unzip_file(directory)
 
-parts_dir = 'finalWebsite/appartement/RandomForestRegressor/models'  # Changez ce chemin si nécessaire
+parts_dir = './finalWebsite/appartement/RandomForestRegressor/models'  # Changez ce chemin si nécessaire
 
 # Remplacez par le bon préfixe et chemin de sortie
 parts_prefix = 'Auvergne-Rhône-Alpes.joblib.part'
-output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Auvergne-Rhône-Alpes.joblib'
+output_file_path = './finalWebsite/appartement/RandomForestRegressor/models/Auvergne-Rhône-Alpes.joblib'
 join_files(parts_prefix, output_file_path, parts_dir)
 # # Vérifie la taille finale du fichier recomposé
 # print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
 #
 parts_prefix = 'Nouvelle-Aquitaine.joblib.part'
-output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Nouvelle-Aquitaine.joblib'
+output_file_path = './finalWebsite/appartement/RandomForestRegressor/models/Nouvelle-Aquitaine.joblib'
 join_files(parts_prefix, output_file_path, parts_dir)
 # # Vérifie la taille finale du fichier recomposé
 # print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
 
 parts_prefix = 'Occitanie.joblib.part'
-output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Occitanie.joblib'
+output_file_path = './finalWebsite/appartement/RandomForestRegressor/models/Occitanie.joblib'
 join_files(parts_prefix, output_file_path, parts_dir)
 # # Vérifie la taille finale du fichier recomposé
 # print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
