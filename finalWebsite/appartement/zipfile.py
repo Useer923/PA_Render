73fee469525029f76de9b79fb7e36c4fa0b9e038
Useer923 <thomastbnlbs@gmail.com>
2024-07-23 16:10:42 +0200

    up-to-date for Render

--- finalWebsite/appartement/zipfile.py
+++ finalWebsite/appartement/zipfile.py
@@ -1,6 +1,21 @@
-import zipfile
 import os
+import zipfile
 
-def unzip_file(zip_file_path, extract_to):
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
+def unzip_file(directory):
+    # Parcourt tous les fichiers dans le répertoire
+    for filename in os.listdir(directory):
+        if filename.endswith('.zip'):
+            zip_path = os.path.join(directory, filename)
+            extract_dir = os.path.join(directory, os.path.splitext(filename)[0])
+
+            # Crée le répertoire pour extraire les fichiers
+            os.makedirs(extract_dir, exist_ok=True)
+            
+            # Décompresse le fichier zip
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_dir)
+                print(f'Extracted {filename} to {extract_dir}')
+            
+            # Supprime le fichier zip après l'extraction
+            os.remove(zip_path)
+            print(f'Deleted {filename}')
--- finalWebsite/finalWebsite/wsgi.py
+++ finalWebsite/finalWebsite/wsgi.py
@@ -9,7 +9,33 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from appartement.zipfile import unzip_file
+from appartement.split_file import join_files
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalWebsite.settings')
 
 application = get_wsgi_application()
+
+directory = 'finalWebsite/appartement/RandomForestRegressor/models'
+unzip_file(directory)
+
+parts_dir = 'finalWebsite/appartement/RandomForestRegressor/models'  # Changez ce chemin si nécessaire
+
+# Remplacez par le bon préfixe et chemin de sortie
+parts_prefix = 'Auvergne-Rhône-Alpes.joblib.part'
+output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Auvergne-Rhône-Alpes.joblib'
+join_files(parts_prefix, output_file_path, parts_dir)
+# # Vérifie la taille finale du fichier recomposé
+# print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
+#
+parts_prefix = 'Nouvelle-Aquitaine.joblib.part'
+output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Nouvelle-Aquitaine.joblib'
+join_files(parts_prefix, output_file_path, parts_dir)
+# # Vérifie la taille finale du fichier recomposé
+# print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
+
+parts_prefix = 'Occitanie.joblib.part'
+output_file_path = 'finalWebsite/appartement/RandomForestRegressor/models/Occitanie.joblib'
+join_files(parts_prefix, output_file_path, parts_dir)
+# # Vérifie la taille finale du fichier recomposé
+# print(f"Taille finale du fichier recomposé: {os.path.getsize(output_file_path)} octets")
